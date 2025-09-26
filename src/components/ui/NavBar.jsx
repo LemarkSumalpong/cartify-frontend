935f7e266b1d6e71f6b6bec2d6520d9d41d60650
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-26 17:00:46 +0800

    reference

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -8,14 +8,20 @@ const NavBar = () => {
         <div className="py-4 shadow-md ">
           <ul className="container mx-auto flex flex-wrap justify-between md:flex-row px-4 md:px-2 items-center relative">
             <div className="flex gap-4 font-semibold">
-              <Link className="text-2xl uppercase font-bold" to="/">
-                cartify
+              <Link
+                className="text-2xl text-white/90 items-center text-justify uppercase font-bold shadow-md"
+                to="/"
+              >
+                <div className="bg-gray-900  w-25 rounded-md">cartify</div>
               </Link>
             </div>
             <div className="flex gap-4 font-semibold ">
               <button>Login</button>
               <button>register</button>
-              <Link to="/cart" className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-800">
+              <Link
+                to="/cart"
+                className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-800"
+              >
                 <ShoppingCart size={20} className=" text-white" />
               </Link>
             </div>
