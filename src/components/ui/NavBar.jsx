f0a1f7900d86f6f09b8d8682fe383d907df3fc3e
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-26 16:29:41 +0800

    reference

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -21,52 +21,14 @@ const NavBar = () => {
                 <Link to="/">Contact</Link>
               </li>
             </div>
-            <div
-              className=
-                "flex flex-col absolute right-0 md:right-0 top-12 z-10 bg-zinc-50 p-4 gap-4 "
-
-                
-                 
+            <div            
             >
-              <li>
-                <Link to={'/login'}>Sign</Link>
-              </li>
-              <li>
-                <Link to="/my-account" >My Account</Link>
-              </li>
-              <User />
+             <button>Login</button>
+             <button>register</button>
             </div>
             
           </ul>
         </div>
-        <nav className="flex justify-between items-center container mx-auto py-2 ">
-          <div className="flex items-center">
-            <Link to="/" className=" py-2 px-4 rounded">
-              <img src={''} alt="PeakStore logo" className="h-12 w-auto" />
-            </Link>
-          </div>
-          <form className="w-1/2  sm:block hidden">
-            <input
-              type="text"
-              placeholder="Search Product..."
-              className="bg-zinc-100100 rounded-md border borders-zinc-100 focus:outline-none py-3 px-3 w-full"
-              
-            />
-          </form>
-          <div className="relative">
-            <Link to={'/cart'}>
-              <ShoppingCart
-                size={50}
-                className="cursor-pointer bg-gray-100 px-3 py-2 rounded-full"
-              />
-              
-                <span className="absolute -top-2 -right-1 bg-red-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
-                
-                </span>
-             
-            </Link>
-          </div>
-        </nav>
       </>
     </header>
   );
