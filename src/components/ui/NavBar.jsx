28d1d63b0fa245c240f26fc95eed5350e4c14270
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-26 16:55:05 +0800

    reference

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,26 +1,28 @@
 import { ShoppingCart } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <header className="bg-white shadow-md">
+    <nav className="bg-white shadow-md">
       <>
         <div className="py-4 shadow-md ">
           <ul className="container mx-auto flex flex-wrap justify-between md:flex-row px-4 md:px-2 items-center relative">
             <div className="flex gap-4 font-semibold">
-             <h1 className='text-2xl uppercase font-bold'>Cartify</h1>
+              <Link className="text-2xl uppercase font-bold" to="/">
+                cartify
+              </Link>
             </div>
             <div className="flex gap-4 font-semibold ">
               <button>Login</button>
               <button>register</button>
-              <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-800">
-              <ShoppingCart size={20} className=' text-white' />
-             </div>
+              <Link to="/cart" className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-800">
+                <ShoppingCart size={20} className=" text-white" />
+              </Link>
             </div>
           </ul>
         </div>
       </>
-    </header>
+    </nav>
   );
 };
 
