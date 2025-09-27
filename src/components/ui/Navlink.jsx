73539a2c1df4022b9894218bcca7cb13a384359b
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-27 22:46:46 +0800

    navlink

--- src/components/ui/Navlink.jsx
+++ src/components/ui/Navlink.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Navlink = () => {
   return (
-    <ul className="hidden md:flex gap-6 font-semibold text-gray-800/90 ">
+    <ul className="hidden md:flex gap-6 font-semibold uppercase text-gray-800/90 ">
     <li>
-        <a href="" className='hover:text-gray-900/90'>Home</a>
+        <a href="#home" className='hover:text-gray-900/90'>Home</a>
     </li>
      <li>
-        <a href="" className='hover:text-gray-900/90'>Shop</a>
+        <a href="#shop" className='hover:text-gray-900/90'>Shop</a>
     </li>
      <li>
-        <a href="" className='hover:text-gray-900/90'>About</a>
+        <a href="#about" className='hover:text-gray-900/90'>About</a>
     </li>
      <li>
-        <a href="" className='hover:text-gray-900/90'>Contact</a>
+        <a href="#contact" className='hover:text-gray-900/90'>Contact</a>
     </li>
    </ul>
   )
