7c29b9ec14894e9c095dfa3382fe1149b4f1dd1d
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-27 21:47:04 +0800

    footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import {
+  ArrowRight,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Mail,
+  Twitter,
+} from 'lucide-react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-slate-900 shadow-md py-4 px-10 ">
+      <div className=" flex px-20 text-white gap-6 justify-center">
+        <a href="#" className="hover:text-blue-400">
+          Home
+        </a>
+        <a href="#" className="hover:text-blue-400">
+          About
+        </a>
+        <a href="#" className="hover:text-blue-400">
+          FAQ
+        </a>
+        <a href="#" className="hover:text-blue-400">
+          Contact
+        </a>
+      </div>
 
-export default Footer
+      <div className="border-t border-gray-700 mt-2">
+        <div className="flex flex-col items-center justify-center py-6 gap-4">
+          <p className="text-blue-400 text-sm">
+            &copy; 2025 Cartify. All rights reserved.
+          </p>
+          <div className="flex gap-4 text-white/70 items-center">
+            <Facebook className="cursor-pointer hover:text-blue-400 transition" />
+            <Linkedin className="cursor-pointer hover:text-blue-500 transition" />
+            <Twitter className="cursor-pointer hover:text-sky-400 transition" />
+            <Instagram className="cursor-pointer hover:text-pink-400 transition" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/ui/NavBar"
+import Footer from "../components/ui/Footer"
 
 const MainLayout = () => {
   return (
    <>
    <NavBar />
    <Outlet />
+   <Footer />
    </>
   )
 }
