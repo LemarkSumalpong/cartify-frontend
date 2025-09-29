d957c99462b5192c18ee10a03f8c209b04389e11
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-29 18:53:51 +0800

    not found

--- src/components/ui/NotFound.jsx
+++ src/components/ui/NotFound.jsx
@@ -5,9 +5,9 @@ const NotFound = () => {
     <header className='py-4 px-4 bg-blue-400'>
         <div className='container px-4 lg:px-4 py-4'>
             <div className='text-center text-white'>
-                <h1>Page not Found</h1>
-                <p>This pages does not exist.</p>
-                <Link to='/' className='rounded-md px-4 py-2'>Home</Link>
+                <h1 className="text-5xl font-semibold">Page not Found</h1>
+                <p>We’re sorry, but the page you’re trying to access cannot be found. It may have been moved, deleted, or is currently unavailable. Please check the URL or return to the homepage.</p>
+                <Link to='/' className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Home</Link>
             </div>
         </div>
     </header>
