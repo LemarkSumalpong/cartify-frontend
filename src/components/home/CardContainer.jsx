653c24db302d585596621701fde4146952740492
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-29 18:30:35 +0800

    cards

--- src/components/home/CardContainer.jsx
+++ src/components/home/CardContainer.jsx
@@ -7,7 +7,7 @@ const CardContainer = () => {
         Our Products
       </h4>
       <div className="container mx-auto px-4 lg:px-4 mt-5">
-        <div className="flex justify-center gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
           <HomeCard />
           <HomeCard />
           <HomeCard />
