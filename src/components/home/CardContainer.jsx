d735c1808b61c34cbe0b8a88af3db1666b8b528e
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-29 18:27:43 +0800

    cards

--- src/components/home/CardContainer.jsx
+++ src/components/home/CardContainer.jsx
@@ -1,9 +1,19 @@
+import HomeCard from './HomeCard';
+
 const CardContainer = () => {
   return (
     <section className="py-5" id="shop">
-      <h4 className="text-center justify-center text-3xl font-semibold text-gray/70">Our Products</h4>
+      <h4 className="text-center justify-center text-3xl font-semibold text-gray/70">
+        Our Products
+      </h4>
       <div className="container mx-auto px-4 lg:px-4 mt-5">
-        <div className="flex justify-center"></div>
+        <div className="flex justify-center px-4">
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+           <HomeCard />
+        </div>
       </div>
     </section>
   );
