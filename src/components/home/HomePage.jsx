9e0868f4aa217265ee238146c3eef8ef39350b29
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-01 20:52:38 +0800

    API

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,8 +1,12 @@
+import api from '../../../api'
 import CardContainer from './CardContainer'
 import Header from './Header'
 import HomeCard from './HomeCard'
 
 const HomePage = () => {
+
+  api.get("products")
+
   return (
    <>
    <Header />
