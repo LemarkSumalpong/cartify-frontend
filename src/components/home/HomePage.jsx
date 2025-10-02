a01703c0137ad17ee41ed466101995a1ce99a38c
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-02 20:31:10 +0800

    products

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import api from '../../../api';
 import CardContainer from './CardContainer';
 import Header from './Header';
 import HomeCard from './HomeCard';
 
 const HomePage = () => {
+
+  const [products, setProducts] = useState([])
   useEffect(function () {
     api.get('products').then((res) => {
       console.log(res.data);
+      setProducts(res.data)
     })
     .catch(err =>{
       console.log(err.message)
