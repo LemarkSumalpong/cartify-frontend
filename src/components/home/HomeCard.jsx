0036718128056808a779fdb55e528c74cd204a04
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-02 20:55:55 +0800

    products

--- src/components/home/HomeCard.jsx
+++ src/components/home/HomeCard.jsx
@@ -16,7 +16,7 @@ const HomeCard = ({product}) => {
           </h3>
 
           <div className="flex justify-center items-center mt-4">
-            <p className="text-yellow-700 font-semibold">{''}₱9,999</p>
+            <p className="text-yellow-700 font-semibold">₱{product.price}</p>
           </div>
         </div>
       </Link>
