63ebed19e5b13db1fb329ae9e556aa73affe075a
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-04 21:52:25 +0800

    products

--- src/components/ui/PlaceHolderContainer.jsx
+++ src/components/ui/PlaceHolderContainer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const PlaceHolderContainer = () => {
   return (
-    <div>PlaceHolderContainer</div>
+    <section className="py-5" id="shop">
+      <h4 className="text-center justify-center text-3xl font-semibold text-gray/70">
+        Our Products
+      </h4>
+      <div className="container mx-auto px-4 lg:px-4 mt-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        </div>
+      </div>
+    </section>
   )
 }
 
