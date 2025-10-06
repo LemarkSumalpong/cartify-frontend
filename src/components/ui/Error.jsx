f8bb0f5171c2f3186d74cb661b17059135b14375
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-06 19:22:15 +0800

    error notif

--- src/components/ui/Error.jsx
+++ src/components/ui/Error.jsx
@@ -7,13 +7,19 @@ const Error = ({error}) => {
     const hasShownToast = useRef(false);
 
     useEffect(() => {
-        if (error %% !hasShownToast.current){
+        if (error && !hasShownToast.current){
             toast.error(error.message || "Something went wrong");
             hasShownToast.current = true;
         }
     }, [error])
   return (
-    <div>Error</div>
+    <div className='flex flex-col items-center justify-center min-h-screen text-center'>
+        <h1 className="text-3xl font-bold text-red-600">Error Occured</h1>
+         <p className="text-gray-700 mt-2">
+        {error?.message || "We couldn’t load your data."}
+      </p>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </div>
   )
 }
 
