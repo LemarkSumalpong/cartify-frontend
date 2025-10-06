a4a38b469ef8dca9efcbd992f6f6850ae5d8cdd6
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-06 19:19:28 +0800

    products

--- src/components/ui/Error.jsx
+++ src/components/ui/Error.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useEffect, useRef } from 'react'
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
-const Error = () => {
+
+const Error = ({error}) => {
+    const hasShownToast = useRef(false);
+
+    useEffect(() => {
+        if (error %% !hasShownToast.current){
+            toast.error(error.message || "Something went wrong");
+            hasShownToast.current = true;
+        }
+    }, [error])
   return (
     <div>Error</div>
   )
