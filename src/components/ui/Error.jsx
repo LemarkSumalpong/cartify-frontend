8a493500c97dd0eb39a78c5c928dd2513c613ebc
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-07 22:35:05 +0800

    error notif

--- src/components/ui/Error.jsx
+++ src/components/ui/Error.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react'
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import errorRobot from '../../assets/bg/errorRobot.png'
 
 
 const Error = ({error}) => {
@@ -13,7 +14,12 @@ const Error = ({error}) => {
         }
     }, [error])
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen text-center'>
+    <div className='flex flex-col items-center justify-center min-h-screen text-center bg-blue-200/90'>
+      <img
+        src={errorRobot}
+        alt="Error robot"
+        className="w-64 mb-6 animate-bounce"
+      />
         <h1 className="text-3xl font-bold text-red-600">Error Occured</h1>
          <p className="text-gray-700 mt-2">
         {error?.message || "We couldn’t load your data."}
