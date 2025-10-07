4ca67602aa91ac91d0f4d943e400d426beff9e65
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-07 22:22:50 +0800

    error notif

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -31,7 +31,7 @@ const HomePage = () => {
   return (
     <>
       <Header />
-      {error && <Error />}
+      {error && <Error error={error} />}
       {products.length === 0 ? (
         <PlaceHolderContainer />
       ) : (
