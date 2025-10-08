12a6fc1e60f57fce8d389f2002e050df34869c70
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-08 18:53:31 +0800

    product page

--- src/components/products/ProductPage.jsx
+++ src/components/products/ProductPage.jsx
@@ -1,8 +1,11 @@
 const ProductPage = () => {
   return (
    <div>
-    <section>
-        
+    <section className="py-3">
+        <div className="container px-4 lg:px-5 my-5">
+            <div>
+            </div>
+        </div>
     </section>
    </div>
   )
