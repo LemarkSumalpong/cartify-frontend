7055a49958497c7e2c6571cec4dd5a327fecbd9c
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-08 18:57:38 +0800

    product page

--- src/components/products/ProductPage.jsx
+++ src/components/products/ProductPage.jsx
@@ -1,14 +1,19 @@
 const ProductPage = () => {
   return (
-   <div>
-    <section className="py-3">
+    <div>
+      <section className="py-3">
         <div className="container px-4 lg:px-5 my-5">
-            <div>
-            </div>
+          <div class="w-full md:w-1/2">
+            <img
+              className="w-full h-48 object-cover mb-5 md:mb-0"
+              src=""
+              alt=""
+            />
+          </div>
         </div>
-    </section>
-   </div>
-  )
-}
+      </section>
+    </div>
+  );
+};
 
-export default ProductPage
+export default ProductPage;
