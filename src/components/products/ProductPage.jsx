dbbae856d84e31f894698cb15bda960efeb4ad65
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-08 19:25:25 +0800

    product page

--- src/components/products/ProductPage.jsx
+++ src/components/products/ProductPage.jsx
@@ -3,12 +3,16 @@ const ProductPage = () => {
     <div>
       <section className="py-3">
         <div className="container px-4 lg:px-5 my-5">
-          <div class="w-full md:w-1/2">
+          <div className="w-full md:w-1/2">
             <img
               className="w-full h-48 object-cover mb-5 md:mb-0"
               src=""
-              alt=""
+              alt="..."
             />
+          </div>
+           <div className="w-full md:w-1/2">
+           <div className="sm:mb-1">Test</div>
+           <h1 className="text-2xl font-semibold">Text 1</h1>
           </div>
         </div>
       </section>
