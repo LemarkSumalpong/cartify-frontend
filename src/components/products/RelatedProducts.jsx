8ac99293f0b82b1c79376a6a49e7fb6aeeec91a1
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-09 21:07:00 +0800

    product page

--- src/components/products/RelatedProducts.jsx
+++ src/components/products/RelatedProducts.jsx
@@ -2,7 +2,7 @@ const RelatedProducts = () => {
   return (
     <section className="py-3 bg-gray-200">
       <div className="container px-4 mt-3">
-        <h2 className="font-semibold mb-4">Related Products</h2>
+        <h2 className="font-bold text-4xl mb-4 text-center">Related Products</h2>
         <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center"></div>
       </div>
     </section>
